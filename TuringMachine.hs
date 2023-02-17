0d2d1c7565d019c8252c1117cceed79d97261e22
Sone <soophiesong@gmail.com>
2023-02-17 12:32:48 -0800

    type declarations

--- TuringMachine.hs
+++ TuringMachine.hs
@@ -11,15 +11,30 @@ module TuringMachine where
     -- states - some sort of collection of states the machine can be in
     -- symbols - some sort of collection of symbols (like changing the pixel colour?)
     -- actions - the types of actions the machine can do (i.e. move left, right, up, down)
+    -- transitions - from state and symbol to new state and symbol, then do transition
     -- mapWidth - dimension of the canvas
     -- mapHeight - dimension of the canvas
 
     -- I'm assuming that the actions should wrap around the map,
     -- so moving right, past the canvas, will land you on the left side
 
-    -- data State
-    -- data Symbol
-    -- (?) data Action
+    -- ((x, y) pixel value)
+    type State = ((Integer, Integer), Symbol)
+    -- (?) deriving (Eq)
 
-    -- type Dimensions = 
-    -- type TuringMachine = State -> Symbol -> [Action] -> Dimensions -> State
+    -- pixel value
+    type Symbol = Integer -- stub for symbol, figure out what symbol is representing (RGB pixel?)
+    -- (?) 
+
+    -- go from one state to another
+    type Action = State -> State
+
+    -- given a start state, new state, and action to choose the new start state using the bounds of the canvas, 
+    -- return the new start state
+    type Transition = State -> State -> Action -> Dimensions -> State
+
+    -- (height, width)
+    type Dimensions = (Integer, Integer)
+
+    -- go from one state to another using a transition
+    type TuringMachine = State -> [Transition] -> State
