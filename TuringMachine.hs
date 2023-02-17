5a5503d576f18e7836c08f7dc8df650ed20faf83
Sone <soophiesong@gmail.com>
2023-02-17 12:36:48 -0800

    updated TODO

--- TuringMachine.hs
+++ TuringMachine.hs
@@ -1,9 +1,9 @@
 module TuringMachine where
 
     -- TODO:
-    -- state and symbol should probably be custom data types
-    -- action as a function/series of functions
     -- (?) should the turing machine iterate over here or where it is used?
+    -- create some initialization fn
+    -- representation of symbol according to reanimate requirements
 
     -- programs.js in Maxime's code
 
@@ -14,9 +14,6 @@ module TuringMachine where
     -- transitions - from state and symbol to new state and symbol, then do transition
     -- mapWidth - dimension of the canvas
     -- mapHeight - dimension of the canvas
-
-    -- I'm assuming that the actions should wrap around the map,
-    -- so moving right, past the canvas, will land you on the left side
 
     -- ((x, y) pixel value)
     type State = ((Integer, Integer), Symbol)
@@ -29,7 +26,8 @@ module TuringMachine where
     -- go from one state to another
     type Action = State -> State
 
-    -- given a start state, new state, and action to choose the new start state using the bounds of the canvas, 
+    -- given a start state, new state, 
+    -- and action to choose the new start state using the bounds of the canvas (wrap around if out of bounds), 
     -- return the new start state
     type Transition = State -> State -> Action -> Dimensions -> State
 
@@ -38,3 +36,4 @@ module TuringMachine where
 
     -- go from one state to another using a transition
     type TuringMachine = State -> [Transition] -> State
+
