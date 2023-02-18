a5f3c384055e8f2949f57e485ef2b20c34775d34
Sone <soophiesong@gmail.com>
2023-02-18 12:55:44 -0800

    test cases + moveUp

--- TuringMachine.hs
+++ TuringMachine.hs
@@ -4,7 +4,8 @@ module TuringMachine where
     -- (?) should the turing machine iterate over here or where it is used?
     -- representation of symbol according to reanimate requirements
     -- write some test cases for the 4 diff actions + basic init
-    -- figure out how to also intiialize the transitions along with the Turing Machine
+    -- figure out how to also intialize the transitions along with the Turing Machine
+    -- implement wrapping in move using dimensions
 
     -- programs.js in Maxime's code
 
@@ -19,8 +20,14 @@ module TuringMachine where
     -- ((x, y) pixel value)
     -- newtype State = State ((Integer, Integer), Symbol)
     -- (x, y, pixel)
-    type State = (Int, Int, Symbol)
+    -- newtype State = State (Int, Int, Symbol)
+    newtype State = State (Int, Int, Symbol)
+        -- deriving (Show)
     -- (?) deriving (Eq)
+
+    -- show as a triple
+    instance Show State where
+        show (State (a, b, c)) = "(" ++ show a ++ ", " ++ show b ++ ", " ++ show c ++ ")"
 
     -- functions to access the triple
     first :: (a, b, c) -> a
@@ -33,8 +40,12 @@ module TuringMachine where
     third (_, _, c) = c
 
     -- pixel value
-    data Symbol = Integer -- stub for symbol, figure out what symbol is representing (RGB pixel?)
+    newtype Symbol = Symbol Int -- stub for symbol, figure out what symbol is representing (RGB pixel?)
+        -- deriving (Show)
     -- (?) 
+
+    instance Show Symbol where
+        show (Symbol a) = show a
 
     -- go from one state to another
     type Action = State -> State
@@ -57,28 +68,39 @@ module TuringMachine where
     -- moveUp :: Action
     -- moveUp (State state1) = State ((fst (fst state1), snd (fst state1) + 1), snd state1)
     moveUp :: State -> TuringMachine -> State
-    moveUp state1 machine = (machine !! first state1) !! (second state1 + 1)
+    moveUp (State state1) machine = (machine !! (second state1 + 1)) !! first state1
+    -- Test Cases:
+    -- moveUp (State (0, 0, Symbol 0)) [[State (0, 0, Symbol 0), State (1, 0, Symbol 1)], [State (0, 1, Symbol 2), State (1, 1, Symbol 3)]]
+    -- returns/shows (0, 1, 2)
+    -- moveUp (State (1, 0, Symbol 1)) [[State (0, 0, Symbol 0), State (1, 0, Symbol 1)], [State (0, 1, Symbol 2), State (1, 1, Symbol 3)]]
+    -- returns/shows (1, 1, 3)
 
     -- y--
     -- moveDown :: Action
     -- moveDown (State state1) = State ((fst (fst state1), snd (fst state1) - 1), snd state1)
     moveDown :: State -> TuringMachine -> State
-    moveDown state1 machine = (machine !! first state1) !! (second state1 - 1)
+    -- moveDown (State state1) machine = (machine !! first state1) !! (second state1 - 1)
+    moveDown (State state1) machine = (machine !! (second state1 - 1)) !! first state1
+    -- Test Cases:
+    -- moveUp (State (0, 1, Symbol 2)) [[State (0, 0, Symbol 0), State (1, 0, Symbol 1)], [State (0, 1, Symbol 2), State (1, 1, Symbol 3)]]
+    -- returns/shows (0, 0, 0)
+    -- moveUp (State (1, 1, Symbol 3)) [[State (0, 0, Symbol 0), State (1, 0, Symbol 1)], [State (0, 1, Symbol 2), State (1, 1, Symbol 3)]]
+    -- returns/shows (1, 1, 3)
 
     -- x--
     -- moveLeft :: Action
     -- moveLeft (State state1) = State ((fst (fst state1) - 1, snd (fst state1)), snd state1)
     moveLeft :: State -> TuringMachine -> State
-    moveLeft state1 machine = (machine !! (first state1 - 1)) !! second state1
+    moveLeft (State state1) machine = (machine !! (first state1 - 1)) !! second state1
 
     -- x++
     -- moveRight :: Action
     -- moveRight (State state1) = State ((fst (fst state1) + 1, snd (fst state1)), snd state1)
     moveRight :: State -> TuringMachine -> State
-    moveRight state1 machine = (machine !! (first state1 + 1)) !! second state1
+    moveRight (State state1) machine = (machine !! (first state1 + 1)) !! second state1
 
-    -- initialize given the same symbol across the canvas
+
+    -- initialize the Turing Machine given the same symbol across the canvas
     blankInit :: Dimensions -> Symbol -> TuringMachine
     -- blankInit dims symbol = [[symbol] * second dims] * first dims
-    blankInit dims symbol = [[(x, y, symbol) | x <- [0..fst dims]] | y <- [0..snd dims]]
-    -- (?) how to initialize transitions WITH the Turing Machine?
+    blankInit dims symbol = [[State (x, y, symbol) | x <- [0..fst dims]] | y <- [0..snd dims]]
