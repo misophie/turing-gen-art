b0e6db862cfc5efc6e97cc559f6b08628ae93c7e
Sone <soophiesong@gmail.com>
2023-02-18 09:45:23 -0800

    redefining data/types and actions

--- TuringMachine.hs
+++ TuringMachine.hs
@@ -16,11 +16,22 @@ module TuringMachine where
     -- mapHeight - dimension of the canvas
 
     -- ((x, y) pixel value)
-    type State = ((Integer, Integer), Symbol)
+    -- newtype State = State ((Integer, Integer), Symbol)
+    -- (x, y, pixel)
+    type State = (Int, Int, Symbol)
     -- (?) deriving (Eq)
 
+    first :: (a, b, c) -> a
+    first (a, _, _) = a
+
+    second :: (a, b, c) -> b
+    second (_, b, _) = b
+
+    third :: (a, b, c) -> c
+    third (_, _, c) = c
+
     -- pixel value
-    type Symbol = Integer -- stub for symbol, figure out what symbol is representing (RGB pixel?)
+    data Symbol = Integer -- stub for symbol, figure out what symbol is representing (RGB pixel?)
     -- (?) 
 
     -- go from one state to another
@@ -34,25 +45,34 @@ module TuringMachine where
     -- (height, width)
     type Dimensions = (Integer, Integer)
 
-    -- go from one state to another using a transition
-    type TuringMachine = State -> [Transition] -> State
+    -- 
+    -- type TuringMachine = State -> [Transition] -> State
+    type TuringMachine = [[State]]
 
     -- defining different actions
 
     -- y++
-    moveUp :: Action
-    moveUp state1 = ((fst (fst state1), snd (fst state1) + 1), snd state1)
+    -- moveUp :: Action
+    -- moveUp (State state1) = State ((fst (fst state1), snd (fst state1) + 1), snd state1)
+    moveUp :: State -> TuringMachine -> State
+    moveUp state1 machine = (machine !! first state1) !! (second state1 + 1)
 
     -- y--
-    moveDown :: Action
-    moveDown state1 = ((fst (fst state1), snd (fst state1) - 1), snd state1)
+    -- moveDown :: Action
+    -- moveDown (State state1) = State ((fst (fst state1), snd (fst state1) - 1), snd state1)
+    moveDown :: State -> TuringMachine -> State
+    moveDown state1 machine = (machine !! first state1) !! (second state1 - 1)
 
     -- x--
-    moveLeft :: Action
-    moveLeft state1 = ((fst (fst state1) - 1, snd (fst state1)), snd state1)
+    -- moveLeft :: Action
+    -- moveLeft (State state1) = State ((fst (fst state1) - 1, snd (fst state1)), snd state1)
+    moveLeft :: State -> TuringMachine -> State
+    moveLeft state1 machine = (machine !! (first state1 - 1)) !! second state1
 
     -- x++
-    moveRight :: Action
-    moveRight state1 = ((fst (fst state1) + 1, snd (fst state1)), snd state1)
+    -- moveRight :: Action
+    -- moveRight (State state1) = State ((fst (fst state1) + 1, snd (fst state1)), snd state1)
+    moveRight :: State -> TuringMachine -> State
+    moveRight state1 machine = (machine !! (first state1 + 1)) !! second state1
 
 
