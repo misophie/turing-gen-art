285da1183607bc149efbad23e7eb4ac820325583
elizabethearle <86982979+elizabethearle@users.noreply.github.com>
2023-02-27 15:09:46 -0800

    Beth io (#10)
    * get input
    
    * random number generator
    
    ---------
    
    Co-authored-by: BuildTools <unconfigured@null.spigotmc.org>

--- NumberGenerator.hs
+++ NumberGenerator.hs
@@ -1,13 +1,11 @@
 module NumberGenerator where
 
-    -- from Feb 15 lec - for Blackjack
-    -- trand :: IO Bool
-    -- trand = do
-    --     g <- newStdGen
-    --     return (ace_first [min 10 (1 + (abs n) `mod` 13) | n <- (randoms g :: [Int])])
+    import System.IO
+    import System.Random
 
-    -- -- no IO for this function even though it is always used in trand which is IO
-    -- ace_first :: (Eq a, Num a) => [a] -> Bool
-    -- ace_first (1:_) = True
-    -- ace_first (10:_) = False
-    -- ace_first(_:t) = ace_first t
+    -- randInt m enerates a random number between 1 and m
+    randInt :: Int -> IO Int
+    randInt m = 
+        do
+            g <- newStdGen
+            return (fst (randomR (1,m) g))
--- main.hs
+++ main.hs
@@ -1,4 +1,43 @@
 module Main where
-    -- dummy return to avoid errors
-    main :: IO()
-    main = return ()
+
+import System.IO
+import Text.Read
+import Data.Maybe
+
+statesLimit = 3
+symbolsLimit = 3
+
+-- 
+main :: IO (Int,Int)
+main = 
+    do 
+        numStates <- intRequest "How many states do you want your turing machine to have?"
+        numStates <- checkLimit numStates statesLimit
+
+        numSymbols <- intRequest "How many symbols?"
+        numSymbols <- checkLimit numSymbols symbolsLimit
+
+        -- run turing machine
+
+        return (numStates,numSymbols)
+
+checkLimit :: Int -> Int -> IO Int
+checkLimit num limit
+    | num > limit || num < 1 = 
+    do
+        newNum <- intRequest ("Your input must be between 1 and "++(show limit))
+        newNum <- checkLimit newNum limit
+        return newNum
+    | otherwise = return num
+
+intRequest :: String -> IO Int
+intRequest str = 
+    do 
+        putStrLn str
+        ln <- getLine
+        let num = readMaybe ln :: Maybe Int
+        if num == Nothing
+            then do
+                num <- intRequest "Your input must be an integer"
+                return num
+            else return (fromJust num)
